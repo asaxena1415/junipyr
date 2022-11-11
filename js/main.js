77f2b6f187202b0e763d3b86e525f34960d5db5b
Aditya Saxena <aditya@MacBook-Air-2.local>
2022-11-10 19:36:23 -0800

    update code

--- js/main.js
+++ js/main.js
@@ -1,13 +1,59 @@
-$(document).ready(function() {
+$(document).ready(function () {
   // getting value
-  $('#preview').on('click', function() {
+  $('#preview').on('click', function () {
     let promptval = $('#elemprompt').val();
-    if(promptval == '') {
+    if (promptval == '') {
       console.log('value is empty')
-    }else {
+    } else {
       console.log('contain values')
     }
   })
 
-  
-})
+  // preview the default value
+  let editorval = $('#editor').val();
+  $('.previewpane').html(editorval)
+
+});
+
+var editor = CodeMirror.fromTextArea
+  (document.getElementById('editor'), {
+    mode: "text/html",
+    theme: "dracula",
+    lineNumbers: true,
+    autoCloseTags: true
+  }).on('change', editor => {
+    console.log(editor.getValue());
+      document.getElementsByClassName('previewpane')[0].innerHTML = editor.getValue();
+  });
+editor.setSize("250", "150");
+
+function image() {
+  var html = "<img src='' alt='' width='' height=''>" + "\n";
+  CodeValue(html);
+
+}
+
+function CodeValue(val) {
+  CodeMirror.fromTextArea(document.getElementById('editor'), {
+        lineNumbers: true,
+        theme: "dracula"
+    }).setValue(val);
+}
+
+function heading() {
+  var html = "<h1>This is a Heading</h1>" + "\n";
+  CodeValue(html);
+}
+
+function form() {
+  var html = `<form>
+
+<input type="email" placeholder="Email">
+
+<input type="text" placeholder="Name">
+
+<input type="submit" value="Submit">
+
+</form>`;
+  CodeValue(html);
+}
